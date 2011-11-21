dcd63af433ef2b5c6a35c8299ed6054aedefe544
Xu Yuan <xu@informatik.hu-berlin.de>
2011-11-21 03:00:05 +0100

    init context from xml

--- openni/xn.pyx
+++ openni/xn.pyx
@@ -3,25 +3,74 @@ from cpython cimport bool  # or from libcpp cimport bool
 ctypedef int XnUInt8
 ctypedef int XnUInt16
 ctypedef int XnUInt32
+ctypedef char XnChar
+ctypedef XnUInt32 XnStatus
+
+XN_STATUS_OK = 0
 
 cdef extern from "XnCppWrapper.h" namespace "xn":
+    ##### Version #####
     cdef cppclass CVersion "xn::Version":
         bool operator==(CVersion right)
-    CVersion *new_Version "new xn::Version" (XnUInt8, XnUInt8, XnUInt16, XnUInt32)
-    void del_Version "delete" (CVersion *rect)
+
+    CVersion *newVersion "new xn::Version" (XnUInt8, XnUInt8, XnUInt16, XnUInt32)
+    void delVersion "delete" (CVersion *rect)
+
+    ##### ScriptNode #####
+    cdef cppclass CScriptNode "xn::ScriptNode":
+        pass
+
+    CScriptNode *newScriptNode "new xn::ScriptNode" ()
+
+    void delScriptNode "delete" (CScriptNode *node)
+
+    ##### Context #####
+    cdef cppclass CContext "xn::Context":
+        XnStatus Init()
+        XnStatus InitFromXmlFile(XnChar* strFileName, CScriptNode& scriptNode)
+
+    CContext *newContext "new xn::Context" ()
+
+    void delContext "delete" (CContext *context)
 
 
 cdef class Version:
     cdef CVersion *_this
 
     def __cinit__(self, int nMajor, int nMinor, int nMaintenance, int nBuild):
-        self._this = new_Version(nMajor, nMinor, nMaintenance, nBuild)
+        self._this = newVersion(nMajor, nMinor, nMaintenance, nBuild)
 
     def __dealloc__(self):
-        del_Version(self._this)
+        delVersion(self._this)
 
     def __richcmp__(Version self, Version other, int op):
         if op == 2:
             return self._this[0] == other._this[0]
         else:
             raise NotImplemented
+
+
+cdef class ScriptNode:
+    cdef CScriptNode* _this
+
+    def __cinit__(self):
+        self._this = newScriptNode()
+
+    def __dealloc__(self):
+        delScriptNode(self._this)
+
+cdef class Context:
+    cdef CContext *_this
+
+    def __cinit__(self):
+        self._this = newContext()
+
+    def __dealloc__(self):
+        delContext(self._this)
+
+    def Init(self):
+        return self._this.Init()
+
+    def InitFromXmlFile(self, strFileName, ScriptNode scriptNode):
+        cdef char* s = strFileName
+        return self._this.InitFromXmlFile(s, scriptNode._this[0])
--- test.py
+++ test.py
@@ -2,17 +2,25 @@
 from openni import test
 from openni import xn
 
-r = test.Rectangle(1, 2, 3, 4)
-print r
-print "Original area:", r.getArea()
-r.move(1,2)
-print r
-print "Area is invariante under rigid motions:", r.getArea()
-r += test.Rectangle(0,0,1,1)
-print r
-print "Now the aread is:", r.getArea()
+# r = test.Rectangle(1, 2, 3, 4)
+# print r
+# print "Original area:", r.getArea()
+# r.move(1,2)
+# print r
+# print "Area is invariante under rigid motions:", r.getArea()
+# r += test.Rectangle(0,0,1,1)
+# print r
+# print "Now the aread is:", r.getArea()
 
 
 v1 = xn.Version(0, 1, 1, 1)
-v2 = xn.Version(0, 1, 1, 1)
-print v1 == v2
+# v2 = xn.Version(0, 1, 1, 1)
+# print v1 == v2
+
+context = xn.Context()
+ret = context.Init()
+assert ret == xn.XN_STATUS_OK
+
+scriptNode = xn.ScriptNode()
+ret = context.InitFromXmlFile('BasicColorAndDepth.xml', scriptNode)
+assert ret == xn.XN_STATUS_OK
