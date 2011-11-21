78b6856ddc19fd50bb962d88393c3b18923cb971
Xu Yuan <xu@informatik.hu-berlin.de>
2011-11-21 06:25:48 +0100

    remove test module

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-
-test = Extension("openni.test",
-              sources=["openni/test.pyx", 'openni/cpp_rect.cpp'],
-              include_dirs=["."],
-              language="c++")
-
 xn = Extension("openni.xn",
                sources=['openni/xn.pyx'],
                include_dirs=[".", '/usr/include/openni'],
@@ -17,6 +11,6 @@ xn = Extension("openni.xn",
 setup(
   name = 'openni',
   packages=['openni'],
-  ext_modules=[test, xn],
+  ext_modules=[xn],
   cmdclass = {'build_ext': build_ext},
 )
